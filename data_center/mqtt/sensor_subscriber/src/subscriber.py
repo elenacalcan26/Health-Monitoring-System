90e6bf9ddcf88f3ea518612af96ab7298604f110
elenacalcan26 <elena.calcan26@gmail.com>
2023-03-20 21:34:20 +0200

    establish connection between mqtt broker & subscriber

--- data_center/mqtt/sensor_subscriber/src/subscriber.py
+++ data_center/mqtt/sensor_subscriber/src/subscriber.py
@@ -1,5 +1,29 @@
+import paho.mqtt.client as mqtt
+import time
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}", flush=True)
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload), flush=True)
+
 def main():
-    pass
+    global client
+
+    time.sleep(5)
+
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.connect("mqtt-broker", 1883)
+    client.loop_forever()
 
 if __name__ == '__main__':
     main()
