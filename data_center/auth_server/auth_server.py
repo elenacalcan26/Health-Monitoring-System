495d53a51428e3b0256fdb93e399f1f3ee4232cc
elenacalcan26 <elena.calcan26@gmail.com>
2023-04-11 01:54:54 +0300

    implement jwt token validation

--- data_center/auth_server/auth_server.py
+++ data_center/auth_server/auth_server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, Response, jsonify, make_response
 from auth_db_client import *
 import jwt
+from functools import wraps
 
 auth_app = Flask(__name__)
 auth_app.config["SECRET_KEY"] = "super secret key"
@@ -20,6 +21,26 @@ def login():
     jwt_token = jwt.encode(data, auth_app.config["SECRET_KEY"], algorithm='HS256')
 
     return make_response(jsonify({"token": jwt_token}), 201)
+
+@auth_app.route("/validate", methods=["POST"])
+def validate():
+    encoded_jwt = request.headers["Authorization"]
+
+    if not encoded_jwt:
+        return jsonify({"message": "Missing credentials!"}), 401
+
+    encoded_jwt = encoded_jwt.split(" ")[1]
+
+    try:
+        decoded = jwt.decode(
+            encoded_jwt, auth_app.config["SECRET_KEY"], algorithms=["HS256"]
+        )
+    except:
+        return jsonify(
+            {"message": "Not Authorized"}
+        ), 403
+
+    return decoded, 200
 
 if __name__ == '__main__':
     auth_app.run('0.0.0.0', port=5000, debug=True)
