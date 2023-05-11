bda846e5793678f253a0b69aa9ab233febc4b54d
elenacalcan26 <elena.calcan26@gmail.com>
2023-05-11 03:05:05 +0300

    display basic doc profile info

--- doctor_android/app/src/main/java/com/example/healthmonitoringsystem/DoctorProfileActivity.kt
+++ doctor_android/app/src/main/java/com/example/healthmonitoringsystem/DoctorProfileActivity.kt
@@ -6,9 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.healthmonitoringsystem.entities.DocProfile
 
 class DoctorProfileActivity: AppCompatActivity() {
-
-    lateinit var docFullNameTextView: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doctor_profile)
@@ -16,7 +13,19 @@ class DoctorProfileActivity: AppCompatActivity() {
         var bundle = intent.extras
         val docProfile = bundle?.getParcelable<DocProfile>("docProfile")
 
-        docFullNameTextView = findViewById(R.id.user_name_text)
+        docProfile?.let { displayDoctorInfo(it) }
+    }
+
+    fun displayDoctorInfo(docProfile: DocProfile) {
+        var docFullNameTextView: TextView = findViewById(R.id.user_name_text)
+        var docMail: TextView = findViewById(R.id.doc_mail)
+        var docPhone: TextView = findViewById(R.id.doc_phone)
+        var docSpecialization: TextView = findViewById(R.id.doc_specialization)
+        var docHospital: TextView = findViewById(R.id.doc_hospital)
         docFullNameTextView.text = "Name: ${docProfile?.full_name}"
+        docMail.text = "Mail: ${docProfile.work_mail}"
+        docPhone.text = "Phone: ${docProfile.phone}"
+        docSpecialization.text = "Specialization: ${docProfile.specialization}"
+        docHospital.text = "Hospital: ${docProfile.hospital_name}"
     }
 }
