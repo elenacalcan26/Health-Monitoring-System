d9d7c870b729b11ddc8240443bb705a742583762
elenacalcan26 <elena.calcan26@gmail.com>
2023-05-18 02:57:19 +0300

    create monitored measurements data models

--- doctor_android/app/src/main/java/com/example/healthmonitoringsystem/entities/Measurement.kt
+++ doctor_android/app/src/main/java/com/example/healthmonitoringsystem/entities/Measurement.kt
@@ -1,6 +1,28 @@
 package com.example.healthmonitoringsystem.entities
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Measurement(
     val measurementType: String,
     val value: Int
-)
+) : Parcelable {
+
+    constructor(parcel: Parcel): this(
+        parcel.readString()!!,
+        parcel.readInt()
+    )
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(measurementType)
+        dest.writeInt(value)
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<Measurement> {
+        override fun createFromParcel(source: Parcel) = Measurement(source)
+
+        override fun newArray(size: Int): Array<Measurement?> = arrayOfNulls(size)
+    }
+}
--- doctor_android/app/src/main/java/com/example/healthmonitoringsystem/entities/MonitoredMeasurements.kt
+++ doctor_android/app/src/main/java/com/example/healthmonitoringsystem/entities/MonitoredMeasurements.kt
@@ -1,6 +1,35 @@
 package com.example.healthmonitoringsystem.entities
 
+import android.os.Parcel
+import android.os.Parcelable
+
+
 data class MonitoredMeasurements(
     val timestamp: String,
     val values: List<Measurement>
-)
+) : Parcelable {
+
+    constructor(parcel: Parcel): this(
+        parcel.readString()!!,
+        parcel.createTypedArrayList(Measurement.CREATOR)!!
+    )
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(timestamp)
+        dest.writeTypedList(values)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MonitoredMeasurements> {
+        override fun createFromParcel(source: Parcel): MonitoredMeasurements {
+            return MonitoredMeasurements(parcel = source)
+        }
+
+        override fun newArray(size: Int): Array<MonitoredMeasurements?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
