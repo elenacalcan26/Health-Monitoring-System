0783e26a24460b197c36dae902f8480bcc1bc63e
elenacalcan26 <elena.calcan26@gmail.com>
2023-06-04 00:57:14 +0300

    use only varchar data type & fix fk mismatched data types

--- python_backend/system_db/init.sql
+++ python_backend/system_db/init.sql
@@ -4,16 +4,16 @@ USE HealthSystemDB;
 CREATE TABLE IF NOT EXISTS HospitalsAdresses
 (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    street_name NVARCHAR(50),
+    street_name VARCHAR(50),
     number INTEGER,
-    city NVARCHAR(30),
+    city VARCHAR(30),
     zip_code INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS Hospitals
 (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name NVARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL,
     address_id INTEGER,
     phone_number INTEGER,
     CONSTRAINT fk_address_id FOREIGN KEY (address_id)
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS Hospitals
 CREATE TABLE IF NOT EXISTS HospitalDepartments
 (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name NVARCHAR(20),
+    name VARCHAR(20),
     hospital_id INTEGER NOT NULL,
     CONSTRAINT fk_hospital_id FOREIGN KEY (hospital_id)
     REFERENCES Hospitals(id)
@@ -35,8 +35,8 @@ CREATE TABLE IF NOT EXISTS Doctors
 (
     username VARCHAR(30) NOT NULL PRIMARY KEY,
     cnp VARCHAR(13) NOT NULL,
-    first_name NVARCHAR(30) NOT NULL,
-    last_name NVARCHAR(30) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     phone INTEGER,
     work_mail VARCHAR(50) NOT NULL,
     specialization VARCHAR(30) NOT NULL,
@@ -52,12 +52,12 @@ CREATE TABLE IF NOT EXISTS Doctors
 CREATE TABLE IF NOT EXISTS PeopleAddresses
 (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    street_name NVARCHAR(50),
+    street_name VARCHAR(50),
     number VARCHAR(10),
     building VARCHAR(10),
     floor VARCHAR(10),
     apartment VARCHAR(10),
-    city NVARCHAR(30),
+    city VARCHAR(30),
     zip_code INTEGER
 );
 
@@ -65,13 +65,13 @@ CREATE TABLE IF NOT EXISTS People
 (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     cnp VARCHAR(13) NOT NULL,
-    first_name NVARCHAR(30),
-    last_name NVARCHAR(30),
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
     birth_date DATE,
     age INTEGER,
-    gender NVARCHAR(10),
+    gender VARCHAR(10),
     phone INTEGER,
-    email NVARCHAR(30),
+    email VARCHAR(30),
     address_id INTEGER,
 
     CONSTRAINT fk_person_address FOREIGN KEY (address_id)
@@ -81,7 +81,7 @@ CREATE TABLE IF NOT EXISTS People
 
 CREATE TABLE IF NOT EXISTS Devices
 (
-    device_name VARCHAR(20) NOT NULL PRIMARY KEY,
+    device_name VARCHAR(30) NOT NULL PRIMARY KEY,
     type VARCHAR(20),
     mac VARCHAR(20),
 
@@ -91,14 +91,14 @@ CREATE TABLE IF NOT EXISTS Devices
 CREATE TABLE IF NOT EXISTS Diagnoses
 (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    diagnosis NVARCHAR(50),
+    diagnosis VARCHAR(50),
     diagnosis_date DATE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Treatments
 (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    treatment NVARCHAR(50),
+    treatment VARCHAR(50),
     start_date DATE NOT NULL,
     end_date DATE,
     diagnosis_id INTEGER,
@@ -126,7 +126,7 @@ CREATE TABLE IF NOT EXISTS Patients
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     person_id INTEGER NOT NULL,
     doctor_id VARCHAR(30) NOT NULL,
-    device_id VARCHAR(30) NOT NULL,
+    device_id VARCHAR(20) NOT NULL,
     monitoring_start_date DATE,
     diagnosis_id INTEGER NOT NULL,
     treatment_id INTEGER NOT NULL,
@@ -149,7 +149,7 @@ CREATE TABLE IF NOT EXISTS PatientsAllergies
 (
     id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     patient_id INTEGER NOT NULL,
-    allergen NVARCHAR(50),
+    allergen VARCHAR(50),
 
     CONSTRAINT fk_patient_id FOREIGN KEY(patient_id)
     REFERENCES Patients(id)
